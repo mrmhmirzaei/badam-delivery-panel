5572f2e5c9d75649004b298dd133678b7444bcf0
Mr.MHMirzaei <mr.mhmirzaei@gmail.com>
2019-04-01 14:58:45 +0430

    Search by name added

--- src/app/includes/search/search.component.ts
+++ src/app/includes/search/search.component.ts
@@ -12,7 +12,13 @@ export class SearchComponent implements OnInit {
   @Input() input:String = 'defualt';
   @Output() onSelect = new EventEmitter();
   myControl = new FormControl();
-  options: Object[] = [];
+  options: Object[] = [{
+    cardId:1,
+    uid:4311370891,
+    firstname: 'محمد جواد',
+    lastname : 'یاحقی',
+    emnumber : 96111147154031
+  }];
   filteredOptions: Observable<Object[]>;
 
   ngOnInit() {
@@ -28,6 +34,8 @@ export class SearchComponent implements OnInit {
     return this.options.filter(option => {
       if(option['uid'].toString().toLowerCase().includes(filterValue)) return true;
       else if(option['emnumber'].toString().toLowerCase().includes(filterValue)) return true;
+        else if(option['firstname'].toString().toLowerCase().includes(filterValue)) return true;
+        else if(option['lastname'].toString().toLowerCase().includes(filterValue)) return true;
       else{}
     });
   }
